14cfc8b89f5e657d3eed8990d67e608b8eedd121
Surdoiu Tudor <studormarian@yahoo.co.uk>
2016-12-12 01:15:14 +0200

    Added Table for Tab 3

---
+++ src/Model/Simple_Select_1.java
@@ -0,0 +1,10 @@
+package Model;
+
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ * Created by DACUS on 12-Dec-16.
+ */
+public class Simple_Select_1 {
+
+}
--- src/sample/Tab3Controller.java
+++ src/sample/Tab3Controller.java
@@ -4,9 +4,15 @@ package sample;/**
 
 import Model.Angajati;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Tab3Controller {
 
@@ -15,13 +21,34 @@ public class Tab3Controller {
     }
 
 
-    @FXML
-    private TextField fNameField;
-    @FXML
-    private TextField lNameField;
 
-    public void OnCLick() {
-        System.out.println("FUck Yeah");
+    public void OnCLick_1() {
+        try {
+        // Load the fxml file and create a new stage for the popup dialog.
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("Cerere_Simpla_1.fxml"));
+        AnchorPane page = (AnchorPane) loader.load();
+
+        // Create the dialog Stage.
+        Stage dialogStage = new Stage();
+        dialogStage.setTitle("Cerere_1");
+        dialogStage.initModality(Modality.WINDOW_MODAL);
+        dialogStage.initOwner(Main.getPrimaryStage());
+        Scene scene = new Scene(page);
+        dialogStage.setScene(scene);
+
+        // Set the person into the controller.
+        Cerere_Simpla_1 controller = loader.getController();
+        controller.setDialogStage(dialogStage);
+
+        dialogStage.showAndWait();
+
+        return ;
+    } catch (IOException e) {
+        e.printStackTrace();
+        return ;
+    }
+
     }
 
 }
